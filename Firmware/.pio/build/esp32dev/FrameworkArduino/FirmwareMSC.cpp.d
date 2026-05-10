79a2c1a86b86ff0f9647f3f11ee7e0db2cb2f5a0
Rocky Shi <rockyshi0329@gmail.com>
2026-05-10 15:08:02 +1000

    Ignore local build artifacts

--- Firmware/.pio/build/esp32dev/FrameworkArduino/FirmwareMSC.cpp.d
+++
@@ -1,5 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/FirmwareMSC.cpp.o: \
- /Users/rocky_shi/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.cpp \
- /Users/rocky_shi/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.h \
- /Users/rocky_shi/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
- /Users/rocky_shi/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
